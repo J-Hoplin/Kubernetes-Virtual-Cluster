248450b8b65a7255f2cc6231856846cd68411cbf
J-Hoplin <jhoplin7259@gmail.com>
2025-03-20 13:15:46 +0900

    밀어

--- scripts/getMasterToken.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-master_node=$1
-
-multipass exec $master_node -- /bin/bash -c "sudo cat /var/lib/rancher/k3s/server/node-token"
--- scripts/terminateCluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-node_name=$1
-
-multipass delete $node_name
